ee55ca055d65a1a435b925a4bf67e9ad909aa1a2
sychoi <cinnamon1995@naver.com>
2025-03-04 16:11:22 +0900

    add models

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as THREE from 'three';
+import './App.css';
+import { useEffect, useRef } from 'react';
+import * as React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(): React.JSX.Element {
+	const mountRef: React.Ref<HTMLDivElement | null> =
+		useRef<HTMLDivElement | null>(null);
+	const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer({
+		antialias: true,
+	});
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	// renderer.outputColorSpace = THREE.sR
+	renderer.shadowMap.enabled = true;
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const scene: THREE.Scene = new THREE.Scene();
+	const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(
+		75,
+		window.innerWidth / window.innerHeight,
+		0.1,
+		1000,
+	);
+	camera.position.z = 5;
+
+	const init_animate = (): void => {
+		renderer.render(scene, camera);
+	};
+	useEffect(() => {
+		if (!mountRef.current) return;
+
+		mountRef.current.appendChild(renderer.domElement);
+		init_animate();
+
+		// 4. 정리(clean-up) 함수
+		return () => {
+			mountRef.current?.removeChild(renderer.domElement);
+		};
+	}, []);
+
+	return <div ref={mountRef} />;
 }
 
-export default App
+export default App;
